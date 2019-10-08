aaf92b9af4262ec8d5af1f7c7b075bb77cb0bb96
San111 <ghassen_chaabane@hotmail.fr>
2019-10-08 11:30:17 +0200

    Basic Model

--- starting_kit/sample_code_submission/model.py
+++ starting_kit/sample_code_submission/model.py
@@ -22,7 +22,7 @@ class model (BaseEstimator):
         self.num_feat=1
         self.num_labels=1
         self.is_trained=False
-        self.mod = RandomForestRegressor(max_depth=20, random_state=0,  n_estimators=100) # Initalizing the model 
+        # self.mod = RandomForestRegressor(max_depth=20, random_state=0,  n_estimators=100) # Initalizing the model 
     
     def fit(self, X, y):
         '''
@@ -46,7 +46,7 @@ class model (BaseEstimator):
         print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
         if (self.num_train_samples != num_train_samples):
             print("ARRGH: number of samples in X and y do not match!")
-        self.mod.fit(X,y)
+        # self.mod.fit(X,y)
         self.is_trained = True
 
     def predict(self, X):
@@ -68,8 +68,7 @@ class model (BaseEstimator):
             print("ARRGH: number of features in X does not match training data!")
         print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
         y = np.zeros([num_test_samples, self.num_labels])
-        # If you uncomment the next line, you get pretty good results for the Iris data :-)
-        y = self.mod.predict(X)
+        # y = self.mod.predict(X)
         return y
 
     def save(self, path="./"):
